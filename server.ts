93d2aebee3d75731e686ed17e546356b5471640f
unknown <FF633@ccs.local>
2026-05-21 15:43:50 +0800

    first commit

---
+++ server.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import errorHandler from './_middleware/error-handler';
+import accountsController from './accounts/accounts.controller';
+import swaggerDocs from './_helpers/swagger';
+import { initialize } from './_helpers/db';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// allow cors requests from any origin with credentials
+app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+
+// api routes
+app.use('/accounts', accountsController);
+
+//swagger docs route
+app.use('/api-docs', swaggerDocs);
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+
+initialize()
+    .then(() => {
+    app.listen(port, () => console.log('Server listening on port ' + port));
+    })
+    .catch(err => {
+        console.error('DB initialization failed:', err);
+        process.exit(1);
+    });
